8f22ecc6d2d337a81fe238a5120f8140869133e1
Bas <55401336+BasToTheMax@users.noreply.github.com>
2022-11-24 16:01:15 +0100

    typo

--- data/nl-en.js
+++ data/nl-en.js
@@ -2,7 +2,7 @@
 const data = [
   { input: 'Hoi', output: 'Hey' }, { input: 'Hoi.', output: 'Hey.' }, { input: 'Hoi!', output: 'Hey!' }, { input: 'Hoi?', output: 'Hey?' },
   { input: 'Hoi', output: 'Hi' }, { input: 'Hoi.', output: 'Hi.' }, { input: 'Hoi!', output: 'Hi!' }, { input: 'Hoi?', output: 'Hi?' },
-  { input: 'Hallo', output: 'Hello' }, { input: 'Hallo.', output: 'Hello.' }, { input: 'Hallo!', output: 'Hello!' }, { input: 'Hallo?', output: 'Hello?' }
+  { input: 'Hallo', output: 'Hello' }, { input: 'Hallo.', output: 'Hello.' }, { input: 'Hallo!', output: 'Hello!' }, { input: 'Hallo?', output: 'Hello?' },
 
   { input: 'Hoe gaat het', output: 'How are you' }, { input: 'Hoe gaat het.', output: 'How are you.' }, { input: 'Hoe gaat het!', output: 'How are you!' }, { input: 'Hoe gaat het?', output: 'How are you?' },
   { input: 'Hoe gaat het', output: 'How are you doing' }, { input: 'Hoe gaat het.', output: 'How are you doing.' }, { input: 'Hoe gaat het!', output: 'How are you doing!' }, { input: 'Hoe gaat het?', output: 'How are you doing?' }
