6264651aef8220fa87e36edf5fa7042523fcf308
Bas <55401336+BasToTheMax@users.noreply.github.com>
2022-11-24 16:00:52 +0100

    Update nl-en.js

--- data/nl-en.js
+++ data/nl-en.js
@@ -1,11 +1,11 @@
 // create data which will be used for training
 const data = [
-  { input: 'Hoi', output: 'Hey' },
-  { input: 'Hallo', output: 'Hello' },
-  { input: 'Hoi', output: 'Hi' },
+  { input: 'Hoi', output: 'Hey' }, { input: 'Hoi.', output: 'Hey.' }, { input: 'Hoi!', output: 'Hey!' }, { input: 'Hoi?', output: 'Hey?' },
+  { input: 'Hoi', output: 'Hi' }, { input: 'Hoi.', output: 'Hi.' }, { input: 'Hoi!', output: 'Hi!' }, { input: 'Hoi?', output: 'Hi?' },
+  { input: 'Hallo', output: 'Hello' }, { input: 'Hallo.', output: 'Hello.' }, { input: 'Hallo!', output: 'Hello!' }, { input: 'Hallo?', output: 'Hello?' }
 
-  { input: 'Hoe gaat het', output: 'How are you' },
-  { input: 'Hoe gaat het', output: 'How are you doing' }
+  { input: 'Hoe gaat het', output: 'How are you' }, { input: 'Hoe gaat het.', output: 'How are you.' }, { input: 'Hoe gaat het!', output: 'How are you!' }, { input: 'Hoe gaat het?', output: 'How are you?' },
+  { input: 'Hoe gaat het', output: 'How are you doing' }, { input: 'Hoe gaat het.', output: 'How are you doing.' }, { input: 'Hoe gaat het!', output: 'How are you doing!' }, { input: 'Hoe gaat het?', output: 'How are you doing?' }
 ];
 
 module.exports = data;
