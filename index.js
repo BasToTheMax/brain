6058692bea0147cc6487772bfb099f6b16434b06
Bas <55401336+BasToTheMax@users.noreply.github.com>
2022-11-24 15:39:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const brain = require('brain.js');
 const fs = require('fs');
+const tokenizer = require('sbd');
 
 const json = fs.readFileSync('train.json', 'utf-8');
 
@@ -7,7 +8,23 @@ const net = new brain.recurrent.LSTM();
 net.fromJSON(json);
 
 
+const myArgs = process.argv.slice(2);
+console.log('myArgs: ', myArgs);
+const text = myArgs.join(' ');
 
-const test = 'Will you be my friend?';
-const output = net.run(test);
-console.log(`Tense: ${output}`);
+var sentences = tokenizer.sentences(text, {});
+
+var l = sentences.length;
+
+var out;
+out = '';
+
+for (let i=0; i<l; i++) {
+  var output = net.run( sentences[i] );
+  
+  out += `${output}\n`;
+}
+
+console.log(`Input: ${text}`);
+console.log('\n-----------\n');
+console.log(`Output: ${out}`);
