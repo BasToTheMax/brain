cbd994deb004ae70bf77f6fc0836d32b88c92151
Bas <55401336+BasToTheMax@users.noreply.github.com>
2022-11-24 15:32:07 +0100

    Update train.js

--- train.js
+++ train.js
@@ -1,6 +1,22 @@
 const brain = require('brain.js');
 const fs = require('fs');
 
+const myArgs = process.argv.slice(2);
+console.log('myArgs: ', myArgs);
+
+if (!myArgs[0]) {
+  console.log('no lang given');
+  process.exit();
+}
+
+switch (myArgs[0]) {
+  case "nl-en":
+    break;
+  default:
+     console.log('invalid lang given');
+    process.exit();
+}
+
 // create configuration for training
 const config = {
   iterations: 15000,
@@ -10,41 +26,7 @@ const config = {
 };
 
 // create data which will be used for training
-const data = [
-  { input: 'I will write a book.', output: 'future' },
-  { input: 'He will be writing a book.', output: 'future' },
-  { input: 'They will have written the book.', output: 'future' },
-  { input: 'I will not cry.', output: 'future' },
-  { input: 'You will find happiness', output: 'future' },
-  { input: 'I am about to leave.', output: 'future' },
-  { input: 'Will you go out?', output: 'future' },
-  { input: 'I shall leave.', output: 'future' },
-  { input: 'We will rock you.', output: 'future' },
-  { input: 'I shall bring the laptop.', output: 'future' },
-
-  { input: 'Will Smith was late.', output: 'past' },
-  { input: 'I had been to that place.', output: 'past' },
-  { input: 'I was selfish.', output: 'past' },
-  { input: 'We had money.', output: 'past' },
-  { input: 'You were so young!', output: 'past' },
-  { input: 'It was the best day.', output: 'past' },
-  { input: 'What were you saying?', output: 'past' },
-  { input: 'I had been to London.', output: 'past' },
-  { input: 'I should not have left.', output: 'past' },
-  { input: 'What was I thinking?', output: 'past' },
-
-  { input: 'I am here.', output: 'present' },
-  { input: 'Are you eating regularly?', output: 'present' },
-  { input: 'Let me in.', output: 'present' },
-  { input: 'I am running.', output: 'present' },
-  { input: 'Please stop screaming.', output: 'present' },
-  { input: 'I cannot help you', output: 'present' },
-  { input: 'Obey the rules.', output: 'present' },
-  { input: 'Call me a lawyer.', output: 'present' },
-  { input: 'Am I wrong?', output: 'present' },
-  { input: 'Right this way.', output: 'present' },
-];
-
+const data = require(`./data/${lang}.js`);
 
 const network = new brain.recurrent.LSTM();
 network.train(data, config);
